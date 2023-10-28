22bfb11c40a6a6538d2da8304ee62ef06012adaf
DanielBenBassat <daniel.benbassat@gmail.com>
2023-10-28 10:37:01 +0300

    decrypt func

--- save_romeo.py
+++ save_romeo.py
@@ -8,9 +8,9 @@ date= 29.9.2023
 
 import sys
 
-#functions
 
-def encrypt(): #הצפנה
+def encrypt():  #הצפנה
+
     encrypt_dict = {"A": 56, "B": 57, "C": 58, "D": 59, "E": 40, "F": 41, "G": 42, "H": 43, "I": 44, "J": 45, "K": 46,
                     "L": 47, "M": 48,
                     "N": 49, "O": 60, "P": 61, "Q": 62, "R": 63, "S": 64, "T": 65, "U": 66, "V": 67, "W": 68, "X": 69,
@@ -21,26 +21,47 @@ def encrypt(): #הצפנה
                     "y": 96, "z": 97,
                     " ": 98, ",": 99, ".": 100, ";": 101, "'": 102, "?": 103, "!": 104, ":": 105}
 
-    message= input("enter text: ")
+    message = input("enter text: ")
     encrypted_msg = ""
     for i in message:
         encrypted_msg += str(encrypt_dict[i]) + ","
-
-    file= open(r"C:\Users\User\OneDrive\מסמכים\דניאל\פייטון יא\romeo/encryped_msg.txt", "w")
+    file = open(r"C:\Users\User\OneDrive\מסמכים\דניאל\פייטון יא\romeo/encryped_msg.txt", "w")
     file.write(encrypted_msg)
     file.close()
 
 
+def decrypt(): #פיענוח
+    """
+    read the encrypted massage from the file and turm the numbers into the massage
+    :return: the decrypted massage
+    """
+    decrypt_dict = {56: "A", 57: "B", 58: "C", 59: "D", 40: "E", 41: "F", 42: "G", 43: "H", 44: "I", 45: "J", 46: "K",
+                    47: "L", 48: "M",
+                    49: "N", 60: "O", 61: "P", 62: "Q", 63: "R", 64: "s", 65: "T", 66: "U", 67: "V", 68: "W", 69: "X",
+                    10: "Y", 11: "Z",
+                    12: "a", 13: "b", 14: "c", 15: "d", 16: "e", 17: "f", 18: "g", 19: "h", 30: "i", 31: "j", 32: "k",
+                    33: "l", 34: "m",
+                    35: "n", 36: "o", 37: "p", 38: "q", 39: "r", 90: "s", 91: "t", 92: "u", 93: "v", 94: "w", 95: "x",
+                    96: "y", 97: "z",
+                    98: " ", 99: ",", 100: ".", 101: ";", 102: "'", 103: "?", 104: "!", 105: ":"}
 
-
-
+    file = open(r"C:\Users\User\OneDrive\מסמכים\דניאל\פייטון יא\romeo/encryped_msg.txt", "r")
+    encrypted_msg = file.read()
+    file.close()
+    decrypted_msg = ""
+    encrypted_msg = encrypted_msg.split(",")
+    for i in encrypted_msg:
+        if i != '':
+            decrypted_msg += decrypt_dict[int(i)]
+    return decrypted_msg
 
 
 def main():
     if sys.argv[1] == "encrypt":
         encrypt()
+    elif sys.argv[1] == "decrypt":
+        print(decrypt())
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
